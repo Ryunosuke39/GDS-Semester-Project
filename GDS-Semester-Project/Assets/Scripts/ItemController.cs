0e922e9a7ddef8cc4bc015b537684a43d5abde4b
JackyySong <Jiarui.Song-1@student.uts.edu.au>
2023-04-02 15:21:33 +1000

    finish item randomly generated
    Add two Scripts: ItemCoctrollr and ItemSpaw... and add one Gameobject  with three prefabs.

---
+++ GDS-Semester-Project/Assets/Scripts/ItemController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // 在这里可以添加玩家获得道具的逻辑（例如增加生命值、攻击力等）
+
+            Destroy(gameObject);
+        }
+    }
+}
