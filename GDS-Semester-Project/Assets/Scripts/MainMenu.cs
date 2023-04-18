ee0a96147cfb443979ad640694f1acd55d3a2b71
JackyySong <Jiarui.Song-1@student.uts.edu.au>
2023-04-19 07:22:53 +1000

    finish the select level page functions
    1

--- GDS-Semester-Project/Assets/Scripts/MainMenu.cs
+++ GDS-Semester-Project/Assets/Scripts/MainMenu.cs
@@ -7,18 +7,18 @@ public class MainMenu : MonoBehaviour
 {
     public void Level1()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
     public void Level2()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
     public void Level3()
     {
+        SceneManager.LoadScene(3);
+    }
+    public void Level4()
+    {
         SceneManager.LoadScene(4);
     }
-    /*public void Level4()
-    {
-        SceneManager.LoadScene(5);
-    }*/
 }
