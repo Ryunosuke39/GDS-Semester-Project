5c5047420d2de00fb060097fa688fa74afee3005
JackyySong <Jiarui.Song-1@student.uts.edu.au>
2023-04-19 06:41:45 +1000

    Create 2 button for win and lose panel
    win button for restart and next level ; lose button for restart and back menu. also create the scripts and on click envent. I will test it now

---
+++ GDS-Semester-Project/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+    public void LoadLevelMenu()
+    {
+        SceneManager.LoadScene(0); 
+    }
+}
