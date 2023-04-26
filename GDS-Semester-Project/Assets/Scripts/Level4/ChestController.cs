f1377780e2ad363baecf3692d7e579535502e51f
JackyySong <Jiarui.Song-1@student.uts.edu.au>
2023-04-26 11:30:56 +1000

    finish chest and function
    level4, then player can get key by chest

--- GDS-Semester-Project/Assets/Scripts/Level4/ChestController.cs
+++ GDS-Semester-Project/Assets/Scripts/Level4/ChestController.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestController : MonoBehaviour
 {
     public GameObject keyAcquiredPanel;
+    public Button okButton;
     private bool isOpened = false;
+
+    private void Start()
+    {
+        okButton.onClick.AddListener(CloseKeyAcquiredPanel);
+        keyAcquiredPanel.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !isOpened)
         {
             isOpened = true;
-            other.GetComponent<Player>().keyAcquired = true;
+            DoorController doorController = FindObjectOfType<DoorController>();
+            if (doorController != null)
+            {
+                doorController.keyAcquired = true;
+            }
             keyAcquiredPanel.SetActive(true);
-            Destroy(gameObject, 0.1f);
         }
+    }
+
+    private void CloseKeyAcquiredPanel()
+    {
+        keyAcquiredPanel.SetActive(false);
+        Destroy(gameObject, 0.1f);
     }
 }
