e0288bb3d7f805905f41814f70e77c3841bd6f80
Ryunosuke39 <111721351+Ryunosuke39@users.noreply.github.com>
2023-05-21 00:36:31 +1000

    A* extension added for enemy ai, enemy ai created

---
+++ GDS-Semester-Project/Assets/Scripts/Level1Scripts/EnemyAIAttack.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIAttack : MonoBehaviour
+{
+    //public float speed = 3f;
+    public float maxHp = 100f;
+    public float currentHp;
+    public float attackDamage = 34f;
+    public float attackDelay = 1f;
+
+    //private Transform player;
+    private bool isAlive = true;
+    private bool isAttacking = false;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentHp = maxHp;
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        /*
+        if (isAlive && player != null)
+        {
+            direction = (player.position - transform.position).normalized;
+            //transform.Translate(direction * speed * Time.deltaTime);
+
+            //transform.position += new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+
+            if (direction.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (direction.x < 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+        */
+
+
+    }
+
+    private void FixedUpdate()
+    {/*
+        if (isAlive && player != null)
+        {
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+        */
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null && !isAttacking)
+            {
+                isAlive = false;
+                StartCoroutine(AttackPlayer(player));
+            }
+            else
+            {
+                Debug.LogWarning($"Player component not found on collided object: {collision.gameObject.name}");
+
+            }
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (isAlive)
+        {
+            currentHp -= damage;
+            FindObjectOfType<AudioManager>().Play("EnemyInjured");//audio manager
+            if (currentHp <= 0)
+            {
+                isAlive = false;
+                FindObjectOfType<AudioManager>().Play("EnemyDeath");//audio manager
+                Timer.Instance.AddTime(5);
+                Destroy(gameObject);
+            }
+            else
+            {
+                // Knockback effect
+                StartCoroutine(Knockback());
+            }
+        }
+    }
+
+    private IEnumerator Knockback()
+    {
+        // Push enemy back a little bit
+        transform.position -= transform.right * 0.2f;
+
+        // Change color to indicate damage taken
+        GetComponent<SpriteRenderer>().color = Color.red;
+
+        yield return new WaitForSeconds(0.1f);
+
+        // Change color back to normal
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private IEnumerator AttackPlayer(Player player)
+    {
+        isAttacking = true;
+        FindObjectOfType<AudioManager>().Play("EnemyAttack");//audio manager
+        yield return new WaitForSeconds(attackDelay);
+        player.TakeDamage(attackDamage);
+        Destroy(gameObject);
+    }
+}
